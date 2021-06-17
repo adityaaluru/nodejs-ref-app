a3804beef88fffedc7061697718ffe61df9f1674
Aditya Aluru <aditya.aluru@gmail.com>
2021-06-17 14:50:23 +0530

    included express logger middleware

--- common/logger.js
+++ common/logger.js
@@ -1,7 +1,13 @@
+const date = require('date-and-time');
+const os = require('os');
+const process = require('process');
+
 let Logger = class  {
     constructor(logPkg,config) {
         this.logPkg = logPkg;
         this.exceptions = config.exceptions;
+        this.logFormat = config.format;
+        this.hostname = os.hostname();
 
         switch(config.rootLevel){
             case 'DEBUG':
@@ -37,26 +43,125 @@ let Logger = class  {
     }
     debug(msg) {
         if(this.allowDebug || isAllowedThroughException('DEBUG',this.logPkg,this.exceptions)){
-            console.log("DEBUG: "+msg);
+            if(this.logFormat === 'json'){
+                msg = JSON.stringify(this.getLogObject('DEBUG',msg));
+            } else {
+                msg = this.getLogLine('DEBUG',msg);
+            }
+            console.log(msg);
         }
     }
     info(msg) {
         if(this.allowInfo || isAllowedThroughException('INFO',this.logPkg,this.exceptions)){
-            console.log("INFO: "+msg);
+            if(this.logFormat === 'json'){
+                msg = JSON.stringify(this.getLogObject('INFO',msg));
+            } else {
+                msg = this.getLogLine('INFO',msg);
+            }
+            console.log(msg);
         }
     }
     warn(msg) {
         if(this.allowWarn || isAllowedThroughException('WARN',this.logPkg,this.exceptions)){
-            console.log("WARN: "+msg);
+            if(this.logFormat === 'json'){
+                msg = JSON.stringify(this.getLogObject('WARN',msg));
+            } else {
+                msg = this.getLogLine('WARN',msg);
+            }
+            console.log(msg);
         }
     }
     error(msg) {
         if(this.allowError || isAllowedThroughException('ERROR',this.logPkg,this.exceptions)){
-            console.log("ERROR: "+msg);
+            if(this.logFormat === 'json'){
+                msg = JSON.stringify(this.getLogObject('ERROR',msg));
+            } else {
+                msg = this.getLogLine('ERROR',msg);
+            }
+            console.log(msg);
         }
     }
+    getLogLine(logLevel, msg) {
+        var timestamp = getCurrentTimestamp();
+        return timestamp+" "+this.hostname+" "+process.pid+" "+logLevel+" "+this.logPkg+" "+msg;
+    }
+    getLogObject(logLevel, msg) {
+        var logObject = {};
+        logObject.timestamp = getCurrentTimestamp();
+        logObject.hostname = this.hostname;
+        logObject.pid = process.pid;
+        logObject.level = logLevel;
+        logObject.pkg = this.logPkg;
+        logObject.msg = msg;
+        return logObject;
+    }
 };
-let isAllowedThroughException = function(logLevel,logPkg,exceptions){
+
+function expressLogger(config) {
+    return function(req,res,next) {
+        if(config.format === 'json') {
+            var logObject = {};
+            logObject.url = req.url;
+            logObject.remoteip = req.ip;
+            logObject.hostname = req.hostname;
+            logObject.method = req.method;
+            logObject.protocol = req.protocol;
+            logObject.type = 'SYSLOG';
+            if(config.express.logReqHeaders){
+                logObject.reqHeaders = req.Header
+            }
+            switch(config.express.logType){
+                case 'accessonly':
+                    logObject.timestamp = getCurrentTimestamp();
+                    logObject.msg = "Express Middlware Log - Access";
+                    console.log(JSON.stringify(logObject));
+                    next();
+                    break;
+                case 'roundtrip':
+                    logObject.msg = "Express Middlware Log - Round Trip";
+                    var startTime = Date.now();
+                    next();
+                    logObject.timestamp = getCurrentTimestamp();
+                    var endTime = Date.now();
+                    if(config.express.logResHeaders){
+                        logObject.resHeaders = res.getHeaders();
+                    }
+                    logObject.statusCode = res.statusCode;
+                    if(config.express.logLocals){
+                        logObject.locals = res.locals;
+                    }
+                    logObject.startTime = startTime;
+                    logObject.responseTime = (endTime - startTime);
+                    console.log(JSON.stringify(logObject));
+                    break;
+                case 'req-and-res':
+                    logObject.timestamp = getCurrentTimestamp();
+                    logObject.msg = "Express Middlware Log - Request Log";
+                    console.log(JSON.stringify(logObject));
+                    var startTime = Date.now();
+                    next();
+                    logObject.timestamp = getCurrentTimestamp();
+                    logObject.msg = "Express Middlware Log - Response Log";
+                    var endTime = Date.now();
+                    if(config.express.logResHeaders){
+                        logObject.resHeaders = res.getHeaders();
+                    }
+                    logObject.statusCode = res.statusCode;
+                    if(config.express.logLocals){
+                        logObject.locals = res.locals;
+                    }
+                    logObject.responseTime = (endTime - startTime);
+                    console.log(JSON.stringify(logObject));
+                    break;
+            }
+        }
+    }
+}
+function getCurrentTimestamp() {
+    return date.format(new Date(),'YYYY-MM-DDTHH:mm:ss.SSSZ');
+}
+
+function isAllowedThroughException(logLevel,logPkg,exceptions) {
     for(var i=0;i<exceptions.length;i++){
         if(logPkg.match(exceptions[i].package)){
             if(logLevel === exceptions[i].logLevel){
@@ -66,4 +171,5 @@ let isAllowedThroughException = function(logLevel,logPkg,exceptions){
     }
     return false;
 }
-module.exports = Logger;
+module.exports.Logger = Logger;
+module.exports.expressLogger = expressLogger;
--- test.js
+++ test.js
@@ -1,5 +1,21 @@
+const express = require('express');
 const config = require('config');
-const Logger = require('./common/logger.js');
+const logModule = require('./common/logger.js');
 
-logger = new Logger('o.test',config.get('config.log'));
+const app = express();
+const PORT = 3000;
+
+logger = new logModule.Logger('o.test',config.get('config.log'));
 logger.info('testing log');
+
+app.use(logModule.expressLogger(config.get('config.log')))
+
+app.get('/',function(request,response){
+    response.status(404).send('test ground!');
+});
+
+//Start the app
+app.listen(PORT, function(){
+    console.log('Server listening on port '+ PORT);
+});
+
